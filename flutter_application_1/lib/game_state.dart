c3a8e3c52e2892b79f2d6007c7601aba434cf0e7
BrandonTJS <tanjiansin@gmail.com>
2022-02-26 07:20:04 +0800

    initial commit

---
+++ flutter_application_1/lib/game_state.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/animation.dart';
+
+class GameState {
+  List<int> boardArr = [];
+
+  GameState() {
+    boardArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+  }
+
+  List<List<int>> tap(int gestureDetectorIndex) {
+    //handles tile movement logic
+    int tile = boardArr[gestureDetectorIndex];
+
+    //return [[tile-1, 2, 1, 0, 1]];
+    if(tile == 16) {
+      //tap on empty tile
+      return [];
+    }
+
+    int previousTile = tile;
+    int currentTile = -1;
+    List<List<int>> tmpAnimationList = [];
+    List<int> tmpSwapList = [];
+
+    //keep checking X axis until either hit 0 (empty tile) or out of bound, build animation list along the way
+    do {
+      //Check left
+      currentTile = getTileLeft(previousTile);
+      if (currentTile == -1) {
+        break;
+      } else {
+        //Add to animation list
+        int affectedTileIndex = findIndexOfTile(previousTile);
+        int affectedTileRow = affectedTileIndex ~/ 4;
+        int affectedTileColumn = affectedTileIndex % 4;
+        tmpAnimationList = [...tmpAnimationList, [affectedTileIndex, affectedTileColumn-1, 0, 1, 1]];
+        tmpSwapList = [previousTile, ...tmpSwapList];
+
+        if(currentTile == 16) {
+          for (int i = 0; i< tmpSwapList.length; i++) {
+            swapPosition(tmpSwapList[i], 16);
+          }
+          return tmpAnimationList;
+        }
+
+        previousTile = currentTile;
+      }
+    } while (true);
+
+    previousTile = tile;
+    currentTile = -1;
+    tmpAnimationList = [];
+    tmpSwapList = [];
+
+    do {
+      //Check right
+      currentTile = getTileRight(previousTile);
+      if (currentTile == -1) {
+        break;
+      } else {
+        //Add to animation list
+        int affectedTileIndex = findIndexOfTile(previousTile);
+        int affectedTileRow = affectedTileIndex ~/ 4;
+        int affectedTileColumn = affectedTileIndex % 4;
+        tmpAnimationList = [...tmpAnimationList, [affectedTileIndex, affectedTileColumn+1, 0, 1, 1]];
+        tmpSwapList = [previousTile, ...tmpSwapList];
+
+        if(currentTile == 16) {
+          for (int i = 0; i< tmpSwapList.length; i++) {
+            swapPosition(tmpSwapList[i], 16);
+          }
+          return tmpAnimationList;
+        }
+
+        previousTile = currentTile;
+      }
+    } while (true);
+
+    previousTile = tile;
+    currentTile = -1;
+    tmpAnimationList = [];
+    tmpSwapList = [];
+
+    //keep checking Y axis until either hit 0 (empty tile) or out of bound, build animation list along the way
+    do {
+      //Check left
+      currentTile = getTileAbove(previousTile);
+      if (currentTile == -1) {
+        break;
+      } else {
+        //Add to animation list
+        int affectedTileIndex = findIndexOfTile(previousTile);
+        int affectedTileRow = affectedTileIndex ~/ 4;
+        int affectedTileColumn = affectedTileIndex % 4;
+        tmpAnimationList = [...tmpAnimationList, [affectedTileIndex, affectedTileRow - 1, 1, 0, 1]];
+        tmpSwapList = [previousTile, ...tmpSwapList];
+
+        if(currentTile == 16) {
+          for (int i = 0; i< tmpSwapList.length; i++) {
+            swapPosition(tmpSwapList[i], 16);
+          }
+          return tmpAnimationList;
+        }
+
+        previousTile = currentTile;
+      }
+    } while (true);
+
+    previousTile = tile;
+    currentTile = -1;
+    tmpAnimationList = [];
+    tmpSwapList = [];
+
+    do {
+      //Check right
+      currentTile = getTileBelow(previousTile);
+      if (currentTile == -1) {
+        break;
+      } else {
+        //Add to animation list
+        int affectedTileIndex = findIndexOfTile(previousTile);
+        int affectedTileRow = affectedTileIndex ~/ 4;
+        int affectedTileColumn = affectedTileIndex % 4;
+        tmpAnimationList = [...tmpAnimationList, [affectedTileIndex, affectedTileRow + 1, 1, 0, 1]];
+        tmpSwapList = [previousTile, ...tmpSwapList];
+
+        if(currentTile == 16) {
+          for (int i = 0; i< tmpSwapList.length; i++) {
+            swapPosition(tmpSwapList[i], 16);
+          }
+          return tmpAnimationList;
+        }
+
+        previousTile = currentTile;
+      }
+    } while (true);
+
+    //default case where tile is in an unmovable position
+    return [];
+  }
+
+  void printBoard() {
+    String printString = "";
+    for (int i = 0; i< boardArr.length; i++) {
+      if(i != 0 && i%4 == 0) {
+        printString += "\n";
+      }
+      printString += boardArr[i].toString() + "\t";
+    }
+    print(printString);
+  }
+
+  void swapPosition(int tile1, int tile2) {
+    int tile1Index = findIndexOfTile(tile1);
+    int tile2Index = findIndexOfTile(tile2);
+    boardArr[tile2Index] = tile1;
+    boardArr[tile1Index] = tile2;
+  }
+
+  int findIndexOfTile(int tile) {
+    return boardArr.indexOf(tile);
+  }
+
+  int getTileAbove(int tile) {
+    int tileIndex = findIndexOfTile(tile);
+    if(tileIndex > 3) {
+      return boardArr[tileIndex - 4];
+    } else {
+      return -1; //out of bound
+    }
+  }
+
+  int getTileBelow(int tile) {
+    int tileIndex = findIndexOfTile(tile);
+    if(tileIndex <= 11) {
+      return boardArr[tileIndex + 4];
+    } else {
+      return -1; //out of bound
+    }
+  }
+
+  int getTileLeft(int tile) {
+    int tileIndex = findIndexOfTile(tile);
+    if(tileIndex % 4 != 0) {
+      return boardArr[tileIndex - 1];
+    } else {
+      return -1; //out of bound
+    }
+  }
+
+  int getTileRight(int tile) {
+    int tileIndex = findIndexOfTile(tile);
+    if(tileIndex % 4 != 3) {
+      return boardArr[tileIndex + 1];
+    } else {
+      return -1; //out of bound
+    }
+  }
+}
---
+++ flutter_application_1/lib/main_bug.dart
@@ -0,0 +1,262 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/game_state.dart';
+import 'package:rive/rive.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const Puzzle(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Puzzle extends StatefulWidget {
+  const Puzzle({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<Puzzle> createState() => _PuzzleState();
+}
+
+class _PuzzleState extends State<Puzzle> {
+  /// Tracks if the animation is playing by whether controller is running.
+  bool get isPlaying => _controller?.isActive ?? false;
+
+  /// Message that displays when state has changed
+  String stateChangeMessage = '';
+
+  Artboard? _riveArtboard;
+  StateMachineController? _controller;
+  List<SMIInput<bool>>? _moves = [], _rows = [], _columns = [];
+  List<SMIInput<double>>? _indexes = [];
+
+  GameState gs = GameState();
+
+  @override
+  void initState() {
+    print("wtf");
+    super.initState();
+
+    final assets = [
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+      'assets/Draft_5_bg_and_border.riv',
+    ];
+
+    var controller;
+
+    for( int i = 0 ; i < assets.length; i++) { 
+      // Load the animation file from the bundle, note that you could also
+      // download this. The RiveFile just expects a list of bytes.
+      rootBundle.load(assets[i]).then(
+        (data) async {
+          // Load the RiveFile from the binary data.
+          final file = RiveFile.import(data);
+
+          // The artboard is the root of the animation and gets drawn in the
+          // Rive widget.
+          final artboard = file.mainArtboard;
+          controller = StateMachineController.fromArtboard(
+            artboard,
+            'State Machine 1',
+            onStateChange: _onStateChange,
+          );
+          if (controller != null) {
+            artboard.addController(controller);
+            for (int k = 0 ; k<controller.inputs.length; k++) {
+              if(controller.inputs[k].name == "move") {
+                _moves?.add(controller.inputs[k]);
+              } else if (controller.inputs[k].name == "row") {
+                _rows?.add(controller.inputs[k]);
+              } else if (controller.inputs[k].name == "column") {
+                _columns?.add(controller.inputs[k]);
+              } else if (controller.inputs[k].name == "index") {
+                _indexes?.add(controller.inputs[k]);
+              }
+            }
+            print("biibi" + (_indexes?.length).toString());
+          }
+          setState(() => _riveArtboard = artboard);
+        },
+      );
+    }
+  }
+
+  /// Do something when the state machine changes state
+  void _onStateChange(String stateMachineName, String stateName) => setState(
+        () => stateChangeMessage =
+            'State Changed in $stateMachineName to $stateName',
+      );
+
+  Widget buildTileGrid(double tileDimension) {
+    final items = [
+      Rive(artboard: _riveArtboard!,),  //tile 1
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),  //tile 8
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),
+      Rive(artboard: _riveArtboard!,),  //tile 16
+    ];
+
+    List<Widget> stackLayers = List<Widget>.generate(items.length, (index) {
+      return Padding(
+        padding: EdgeInsets.only(left: index%4 * tileDimension, top: index~/4 * tileDimension, bottom: 0, right: 0),
+        child: 
+          Container(
+            height: tileDimension,
+            width: tileDimension,
+            child: items[index]
+          ),
+      );
+    });
+
+    return Stack(children: stackLayers);
+  }
+
+  Widget buildGestureGrid(double tileDimension) { 
+    var rng = Random();
+    print("wah" + rng.nextInt(100).toString());
+    List<Widget> stackLayers = List<Widget>.generate(16, (index) {
+      return Padding(
+        padding: EdgeInsets.only(left: index%4 * tileDimension, top: index~/4 * tileDimension, bottom: 0, right: 0),
+        child: 
+          GestureDetector(
+            onTapDown: (_) {
+              List<List<int>> animationPlaylist = gs.tap(index);
+              for(int i=0;i<animationPlaylist.length;i++) {
+                
+ 
+                  _indexes?[i].value = animationPlaylist[i][0].toDouble();
+                  print("yo" + (_indexes?[i].value).toString());
+                  _rows?[i].value = animationPlaylist[i][1] == 1 ? true : false;
+                  _columns?[i].value = animationPlaylist[i][2] == 1 ? true : false;
+                  _moves?[i].value = animationPlaylist[i][3] == 1 ? true : false;
+
+              }
+            },
+            child:
+            Opacity(
+              opacity: 0.3,
+              child: Container(
+                height: tileDimension,
+                width: tileDimension,
+                color: Colors.pink,
+              )
+            ),
+          ),
+      );
+    });
+
+    return Stack(children: stackLayers);
+  } 
+
+  Widget buildPlayGrid(double gridWidth, double gridHeight){
+    //Calculate tile dimensions
+    double dimensionLimit = min(gridWidth, gridHeight);
+    double tileDimension = dimensionLimit / 4;
+
+    return Stack(children: 
+    [
+      buildTileGrid(tileDimension), 
+      buildGestureGrid(tileDimension),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    print("sw:" + screenWidth.toString());
+    print("sh:" + screenHeight.toString());
+
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text('Puzzle'),
+      ),
+      body: Center(
+        child: _riveArtboard == null
+            ? const SizedBox()
+            :  Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Press to activate!',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: buildPlayGrid(screenWidth, screenHeight),
+                    ),
+                    const SizedBox(height: 10),
+                    Text('$stateChangeMessage'),
+                    const SizedBox(height: 10),
+                    
+                  ],
+                ),
+      ),
+    );
+  }
+}
