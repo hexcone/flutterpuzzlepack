e6dd52aa72f96c1c1f54d50b1762dfeab393be26
Brandon Tan Jian Sin <tanjiansin@gmail.com>
2022-02-27 17:28:51 +0800

    add winning star animation

--- flutter_application_1/lib/win_screen.dart
+++ flutter_application_1/lib/win_screen.dart
@@ -16,6 +16,7 @@ class WinScreen extends StatefulWidget {
 
 class _WinScreenState extends State<WinScreen> {
   GameState? gs;
+  Artboard? _riveArtboardWinningStar;
 
   _WinScreenState(GameState gs){
     this.gs = gs;
@@ -24,9 +25,25 @@ class _WinScreenState extends State<WinScreen> {
   @override
   void initState() {
     super.initState();
+    rootBundle.load('assets/winningstar.riv').then(
+          (data) async {
+        // Load the RiveFile from the binary data.
+        final file = RiveFile.import(data);
+
+        // The artboard is the root of the animation and gets drawn in the
+        // Rive widget.
+        final artboard = file.mainArtboard;
+        var controller = SimpleAnimation('Animation 1');
+        if (controller != null) {
+          artboard.addController(controller);
+        }
+        setState(() => _riveArtboardWinningStar = artboard);
+      },
+    );
   }
 
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
 
     return 
       WillPopScope(
@@ -51,7 +68,18 @@ class _WinScreenState extends State<WinScreen> {
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                Text.rich(TextSpan(text: 'You Won!', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255)))),
+                                _riveArtboardWinningStar == null
+                                ? const SizedBox()
+                                : SizedBox(
+                                    height: screenHeight * 0.3,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Rive(
+                                        artboard: _riveArtboardWinningStar!,
+                                      ),
+                                    ),
+                                  ),
+                                Text.rich(TextSpan(text: 'You Won!', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255), fontSize: 50))),
                                 Text.rich(TextSpan(text: 'Time Taken: ' + gs!.getTimeTakenString(), style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255)))),
                                 Text.rich(TextSpan(text: 'Number of Moves: ' + gs!.getNumMoves(), style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255)))),
                               ],
