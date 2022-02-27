cc1ce1253c8e19c774f354085dc2be03debf4807
Brandon Tan Jian Sin <tanjiansin@gmail.com>
2022-02-27 14:48:27 +0800

    fix a bug where default volume for sound asset not set

--- flutter_application_1/lib/audio_manager.dart
+++ flutter_application_1/lib/audio_manager.dart
@@ -37,9 +37,7 @@ class _AudioPlayerState extends State<AudioPlayerWidget> {
     print("play bg music");
     advancedPlayer = AudioPlayer();
     player = AudioCache(fixedPlayer: advancedPlayer);
-    player!.loop("audio/NatureSample.mp3"); //https://www.ashamaluevmusic.com/ambient-music
-    advancedPlayer!.setVolume(DEFAULT_VOLUME);
-
+    player!.loop("audio/NatureSample.mp3").then((value) => value.setVolume(DEFAULT_VOLUME)); //https://www.ashamaluevmusic.com/ambient-music
   }
 
   Widget buildAudioButton() {
