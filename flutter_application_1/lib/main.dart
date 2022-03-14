f9bfa2cb8cbe71f0c95f9fd31cb53301df1b97f1
Brandon Tan Jian Sin <tanjiansin@gmail.com>
2022-03-14 14:46:34 +0800

    fix windows padding

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -178,10 +178,10 @@ class _LanguageTilesStateMachineState extends State<LanguageTilesStateMachine> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     int extraTopPadding = 0;
-    if (defaultTargetPlatform == TargetPlatform.android ||defaultTargetPlatform == TargetPlatform.windows) {
+    if (defaultTargetPlatform == TargetPlatform.android ||defaultTargetPlatform == TargetPlatform.iOS) {
       extraTopPadding = 100;
     }
-    else if (defaultTargetPlatform == TargetPlatform.linux || defaultTargetPlatform == TargetPlatform.macOS || defaultTargetPlatform == TargetPlatform.iOS) {
+    else if (defaultTargetPlatform == TargetPlatform.linux || defaultTargetPlatform == TargetPlatform.macOS || defaultTargetPlatform == TargetPlatform.windows) {
       extraTopPadding = 0;
     }
     else {
